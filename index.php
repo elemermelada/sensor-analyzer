f23ef83cfe9210e3f828f03fd2a3a2b14023c640
Elemer San Miguel Niddam <esanmiguel@zenztech.com>
2024-04-17 19:20:36 +0200

    better tests

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/util.php';
 
 $input = $argv[1];
 $grid = new Grid($input);
-var_dump($grid->is_cell_active(0, 0));
-var_dump($grid->is_cell_active(0, 1));
+var_dump($grid->is_cell_active(-1, 0));
+var_dump($grid->is_cell_active(0, 4));
 var_dump($grid->is_cell_active(1, 0));
 var_dump($grid->is_cell_active(1, 1));
